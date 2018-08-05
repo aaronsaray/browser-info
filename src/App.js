b168d6cc25127047147f541ee4efc783e3594a09
Aaron Saray <me@aaronsaray.com>
2018-08-05 14:28:43 -0500

    added prop types and moving around items

--- src/App.js
+++ src/App.js
@@ -3,20 +3,62 @@ import { Container } from "semantic-ui-react";
 import LoadingIndicator from "./components/LoadingIndicator/LoadingIndicator";
 import Results from "./components/Results/Results";
 import styles from "./App.css";
+import ChromeImage from "./images/chrome.svg";
+import AppleImage from "./images/apple.svg";
 
 class App extends React.Component {
   state = {
-    resultsLoaded: true
+    resultsLoaded: false,
+    browser: null,
+    browserDetailed: null,
+    browserImage: null,
+    operatingSystem: null,
+    operatingSystemDetailed: null,
+    operatingSystemImage: null,
+    ipPublic: null,
+    ipPrivate: null
   };
 
   componentDidMount = () => {
-    console.log("mounted");
+    const browser = "Google Chrome";
+    const browserDetailed = "65.4.4";
+    const browserImage = ChromeImage;
+    const operatingSystem = "MacOS";
+    const operatingSystemDetailed = "10.6.4";
+    const operatingSystemImage = AppleImage;
+    const ipPublic = "65.128.0.4";
+    const ipPrivate = "127.0.0.1";
+
+    this.setState({
+      resultsLoaded: true,
+      browser,
+      browserDetailed,
+      browserImage,
+      operatingSystem,
+      operatingSystemDetailed,
+      operatingSystemImage,
+      ipPublic,
+      ipPrivate
+    });
   };
 
   render() {
     return (
       <Container className={styles.container}>
-        {this.state.resultsLoaded ? <Results /> : <LoadingIndicator />}
+        {this.state.resultsLoaded ? (
+          <Results
+            browser={this.state.browser}
+            browserDetailed={this.state.browserDetailed}
+            browserImage={this.state.browserImage}
+            operatingSystem={this.state.operatingSystem}
+            operatingSystemDetailed={this.state.operatingSystemDetailed}
+            operatingSystemImage={this.state.operatingSystemImage}
+            ipPublic={this.state.ipPublic}
+            ipPrivate={this.state.ipPrivate}
+          />
+        ) : (
+          <LoadingIndicator />
+        )}
       </Container>
     );
   }
--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,40 +1,66 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Grid, Card, Image, Header } from "semantic-ui-react";
-import ChromeImage from "../../images/chrome.svg";
-import AppleImage from "../../images/apple.svg";
 import IPImage from "../../images/internet.svg";
 
-const ResultCard = (image, header, meta) => {
+const ResultCard = (image, header, details, description) => {
   return (
     <Card fluid>
       <Card.Content>
         <Image src={image} floated="left" />
         <Card.Header>{header}</Card.Header>
-        <Card.Meta>{meta}</Card.Meta>
+        <Card.Meta>{details}</Card.Meta>
+        <Card.Description>{description}</Card.Description>
       </Card.Content>
     </Card>
   );
 };
 
-const Results = () => (
+const Results = props => (
   <React.Fragment>
     <Header as="h1" className="center aligned">
       Browser Information
     </Header>
-    <Grid columns={3}>
+    <Grid stackable columns={3}>
       <Grid.Row>
         <Grid.Column>
-          {ResultCard(ChromeImage, "Google Chrome", "Your Internet Browser")}
+          {ResultCard(
+            props.browserImage,
+            props.browser,
+            props.browserDetailed,
+            "Your Internet Browser"
+          )}
         </Grid.Column>
         <Grid.Column>
-          {ResultCard(AppleImage, "MacOS", "Your Operating System")}
+          {ResultCard(
+            props.operatingSystemImage,
+            props.operatingSystem,
+            props.operatingSystemDetailed,
+            "Your Operating System"
+          )}
         </Grid.Column>
         <Grid.Column>
-          {ResultCard(IPImage, "127.0.0.1", "Your IP Address")}
+          {ResultCard(
+            IPImage,
+            props.ipPublic,
+            props.ipPrivate,
+            "Your IP Address"
+          )}
         </Grid.Column>
       </Grid.Row>
     </Grid>
   </React.Fragment>
 );
+
+Results.propTypes = {
+  browser: PropTypes.string.isRequired,
+  browserDetailed: PropTypes.string.isRequired,
+  browserImage: PropTypes.string.isRequired,
+  operatingSystem: PropTypes.string.isRequired,
+  operatingSystemDetailed: PropTypes.string.isRequired,
+  operatingSystemImage: PropTypes.string.isRequired,
+  ipPublic: PropTypes.string.isRequired,
+  ipPrivate: PropTypes.string.isRequired
+};
 
 export default Results;
