ff355f48df65b1b1010dd552cf6c74e39a3cd35c
Aaron Saray <me@aaronsaray.com>
2018-08-04 20:29:59 -0500

    Linear progress added

--- src/components/LoadingIndicator/LoadingIndicator.js
+++ src/components/LoadingIndicator/LoadingIndicator.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import LinearProgress from "@material-ui/core/LinearProgress";
 import "./LoadingIndicator.css";
 
 const LoadingIndicator = () => (
-  <Grid container className="loading-indicator">
+  <Grid container className="loading-indicator" spacing={16}>
     <Grid item xs={12}>
       <p>Here is a loading indicator.</p>
+      <LinearProgress />
     </Grid>
   </Grid>
 );
