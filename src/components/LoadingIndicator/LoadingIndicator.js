446de340fa2ef345454352a514e33410d2fad3cb
Aaron Saray <me@aaronsaray.com>
2018-08-25 16:38:43 -0500

    removed an unneeded fragment

--- src/components/LoadingIndicator/LoadingIndicator.js
+++ src/components/LoadingIndicator/LoadingIndicator.js
@@ -2,11 +2,9 @@ import React from "react";
 import { Loader } from "semantic-ui-react";
 
 const LoadingIndicator = () => (
-  <React.Fragment>
-    <Loader active inline="centered" size="massive">
-      Analyzing Browser
-    </Loader>
-  </React.Fragment>
+  <Loader active inline="centered" size="massive">
+    Analyzing Browser
+  </Loader>
 );
 
 export default LoadingIndicator;
