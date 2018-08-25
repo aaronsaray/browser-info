673955dca0c7ae8b54bd8746d564b98f0e93e274
Aaron Saray <me@aaronsaray.com>
2018-08-25 17:11:31 -0500

    adding more tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,15 @@
 import React from "react";
 import App from "./App";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 describe("App", () => {
   it("renders", () => {
     shallow(<App />);
+  });
+
+  it("shows loading indicator at first", () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.text()).toContain("Analyzing Browser");
+    wrapper.unmount();
   });
 });
